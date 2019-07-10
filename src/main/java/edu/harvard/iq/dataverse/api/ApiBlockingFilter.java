2ce1d48a8696ded36871ac31cdb26bfefbc782ef
Victoria Lubitch <victoria.lubitch@utoronto.ca>
2019-07-10 16:10:53 -0400

    add Access-Control-Allow-Headers

--- src/main/java/edu/harvard/iq/dataverse/api/ApiBlockingFilter.java
+++ src/main/java/edu/harvard/iq/dataverse/api/ApiBlockingFilter.java
@@ -161,7 +161,7 @@ public class ApiBlockingFilter implements javax.servlet.Filter {
         try {
             ((HttpServletResponse) sr1).addHeader("Access-Control-Allow-Origin", "*");
             ((HttpServletResponse) sr1).addHeader("Access-Control-Allow-Methods","PUT");
-            ((HttpServletResponse) sr1).addHeader("allowedHeaders", "Content-Type");
+            ((HttpServletResponse) sr1).addHeader("Access-Control-Allow-Headers", "Content-Type");
             fc.doFilter(sr, sr1);
         } catch ( ServletException se ) {
             logger.log(Level.WARNING, "Error processing " + requestURI +": " + se.getMessage(), se);
