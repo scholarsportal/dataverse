c0a94de2021599bb1b1e17fcab1f24b4b2c3d195
Victoria Lubitch <victoria.lubitch@utoronto.ca>
2019-11-26 15:19:43 -0500

    create directory

--- src/main/java/edu/harvard/iq/dataverse/globus/MkDir.java
+++ src/main/java/edu/harvard/iq/dataverse/globus/MkDir.java
@@ -1,19 +1,19 @@
 package edu.harvard.iq.dataverse.globus;
 
 public class MkDir {
-    private String DATA_TYPE;
+    private String dataType;
     private String path;
 
-    public void setDATA_TYPE(String DATA_TYPE) {
-        this.DATA_TYPE = DATA_TYPE;
+    public void setDataType(String dataType) {
+        this.dataType = dataType;
     }
 
     public void setPath(String path) {
         this.path = path;
     }
 
-    public String getDATA_TYPE() {
-        return DATA_TYPE;
+    public String getDataType() {
+        return dataType;
     }
 
     public String getPath() {
